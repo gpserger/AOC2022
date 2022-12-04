201cd577df41ec170dafa3533fb96ef646a014ce
Gustav Serger <gustav.serger@gmail.com>
2022-12-04 15:56:24 +0100

    day 4 part 2 done

--- Day 4/README.md
+++ Day 4/README.md
@@ -3,3 +3,5 @@
 Had an issue in part 1 where the code worked for the sample but not the whole input.
 Changing to using python ranges solved it.
 
+Part 2 was just overlap so easier than part 1 in my opinion.
+Not sure any of the puzzles so far are harder than any other.
--- Day 4/day 4.py
+++ Day 4/day 4.py
@@ -21,3 +21,17 @@ for pair in data:
         sum += 1
 
 print(sum)
+
+# part 2
+sum = 0
+for pair in data:
+    elf1, elf2 = pair.split(",")
+    elf1bounds = elf1.split("-")
+    elf2bounds = elf2.split("-")
+    elf1range = range(int(elf1bounds[0]), int(elf1bounds[1]) + 1)
+    elf2range = range(int(elf2bounds[0]), int(elf2bounds[1]) + 1)
+    if elf1range.start in elf2range or elf1range[-1] in elf2range:
+        sum += 1
+    elif elf2range.start in elf1range or elf2range[-1] in elf1range:
+        sum += 1
+print(sum)
