d08fb16ef26818f880897807c5beb5ba5e1b3515
Gustav Serger <gustav.serger@gmail.com>
2022-12-04 15:52:55 +0100

    day 4 part 1 done

---
+++ Day 4/README.md
@@ -0,0 +1,5 @@
+# Reflections
+
+Had an issue in part 1 where the code worked for the sample but not the whole input.
+Changing to using python ranges solved it.
+
---
+++ Day 4/day 4.py
@@ -0,0 +1,23 @@
+with open("data/input") as f:
+    data = f.readlines()
+    data = [x.strip() for x in data]
+
+def range_subset(range1, range2):
+    """Whether range1 either of range2 is a subset of the other"""
+    if range1.start in range2 and range1[-1] in range2:
+        return True
+    if range2.start in range1 and range2[-1] in range1:
+        return True
+
+
+# part 1
+sum = 0
+for pair in data:
+    pass
+    elf1, elf2 = pair.split(",")
+    elf1bounds = elf1.split("-")
+    elf2bounds = elf2.split("-")
+    if range_subset(range(int(elf1bounds[0]), int(elf1bounds[1]) + 1), range(int(elf2bounds[0]), int(elf2bounds[1]) + 1)):
+        sum += 1
+
+print(sum)
