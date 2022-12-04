54be65cac4e18727312fb2a661e0ff2773bb8b6a
Gustav Serger <gustav.serger@gmail.com>
2022-12-04 15:33:14 +0100

    day 3 part 2 done

---
+++ Day 3/README.md
@@ -0,0 +1,6 @@
+# Reflections
+
+Pretty easy, just working with strings as arrays of characters.
+
+I used a string to look up priority values.
+i.e. "abc...xyzABC...XYZ" and then used the index of the character in the string + 1 to determine the priority.
--- Day 3/day 3.py
+++ Day 3/day 3.py
@@ -1,12 +1,13 @@
 with open("data/input1") as f:
     data = f.readlines()
+    data = [x.strip() for x in data]
 
 
 priorities = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+# part 1
 total = 0
 for rucksack in data:
-    rucksack = rucksack.strip()
     compartment1 = set(rucksack[:len(rucksack)//2])
     compartment2 = rucksack[len(rucksack)//2:]
     # Find sum of common letter priorities
@@ -18,6 +19,16 @@ for rucksack in data:
     total += sum
 print(total)
 
+# part 2
+# every 3 rucksacks is a new group
+total = 0
+groups = [data[i:i+3] for i in range(0, len(data), 3)]
+for group in groups:
+    # find only common letter in all 3 rucksacks
+    common = set(group[0])
+    for letter in common:
+        if letter in group[1] and letter in group[2]:
+            total += priorities.index(letter) + 1
+            break
 
-
-
+print(total)
